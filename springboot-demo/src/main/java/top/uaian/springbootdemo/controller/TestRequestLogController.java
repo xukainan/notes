29d96755e117de49d1b7494b4bfe08209d076c89
kainan <836127990@qq.com>
2020-03-29 20:24:41 +0800

    测试跨域注解：@CrossOrigin

--- springboot-demo/src/main/java/top/uaian/springbootdemo/controller/TestRequestLogController.java
+++ springboot-demo/src/main/java/top/uaian/springbootdemo/controller/TestRequestLogController.java
@@ -1,5 +1,6 @@
 package top.uaian.springbootdemo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/test")
+@CrossOrigin
 public class TestRequestLogController {
 
     @RequestMapping("/request_log")
