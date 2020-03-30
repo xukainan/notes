dc9614f5e34d24a6ae7e8bb5ccc3f1d736961f4f
xukainan <xukainan@fang.com>
2020-03-30 15:57:00 +0800

    创建mybatisplus-demo

---
+++ mybatisplus-demo/src/main/java/top/uaian/mybatisplus/dbconfig/DataSourceConfig.java
@@ -0,0 +1,10 @@
+package top.uaian.mybatisplus.dbconfig;
+
+/**
+ * description:  数据源配置<br>
+ * date: 2020/3/30 15:33 <br>
+ * @author: xukainan <br>
+ * version: 1.0 <br>
+ */
+public class DataSourceConfig {
+}
--- springboot-demo/src/main/java/top/uaian/springbootdemo/conf/CorsConfig.java
+++ springboot-demo/src/main/java/top/uaian/springbootdemo/conf/CorsConfig.java
@@ -1,33 +1,33 @@
-package top.uaian.springbootdemo.conf;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-/**
- * description:  全局跨域配置<br>
- * date: 2020/3/30 9:04 <br>
- * @author: xukainan <br>
- * version: 1.0 <br>
- */
-@Configuration
-public class CorsConfig {
-    private CorsConfiguration corsConfiguration(){
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod(HttpMethod.GET);
-        corsConfiguration.addAllowedMethod(HttpMethod.POST);
-        corsConfiguration.addAllowedOrigin("*");
-        return corsConfiguration;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration());
-        return new CorsFilter(urlBasedCorsConfigurationSource);
-    }
-}
+//package top.uaian.springbootdemo.conf;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+///**
+// * description:  全局跨域配置<br>
+// * date: 2020/3/30 9:04 <br>
+// * @author: xukainan <br>
+// * version: 1.0 <br>
+// */
+//@Configuration
+//public class CorsConfig {
+//    private CorsConfiguration corsConfiguration(){
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+//        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+//        corsConfiguration.addAllowedOrigin("*");
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration());
+//        return new CorsFilter(urlBasedCorsConfigurationSource);
+//    }
+//}
