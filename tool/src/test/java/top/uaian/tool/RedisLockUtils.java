7fa9fe682a40c8e33289011a5f2f517f37291344
xukainan <xukainan@fang.com>
2020-04-03 13:59:12 +0800

    Redis实现分布式锁

---
+++ tool/src/test/java/top/uaian/tool/RedisLockUtils.java
@@ -0,0 +1,55 @@
+package top.uaian.tool;
+
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * description:  Redis分布式锁简单实现<br>
+ * date: 2020/4/3 11:21 <br>
+ * @author: xukainan <br>
+ * version: 1.0 <br>
+ */
+public class RedisLockUtils {
+
+    static StringRedisTemplate stringRedisTemplate;
+
+    private static final long expireTime = 60;
+
+    public static boolean lock(String key){
+        long time = new Date().getTime();
+        String value = String.valueOf(time + expireTime);
+        Boolean isSuccess = stringRedisTemplate.opsForValue().setIfAbsent(key, value);
+//        if(isSuccess) {
+//            stringRedisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+//            return true;
+//        }
+        //处理“锁已经过期，但是没有运行解锁操作”
+        //假设获取的值是a
+        String currentValue = stringRedisTemplate.opsForValue().get(key);
+        //当key存在并且已经过期
+        if(StringUtils.isNotBlank(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()){
+            //处理多个线程同时进入的情况
+            //A线程get旧值a， set新值b
+            //B线程get旧值b， set新值b
+            String oldValue = stringRedisTemplate.opsForValue().getAndSet(key, value);
+            //A线程oldvalue = a， currentValue =a 进入方法
+            //B线程oldvalue = b， currentValue =a 不进入方法
+            if(StringUtils.isNotBlank(oldValue) && oldValue.equals(currentValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean unlock(String key, String value){
+        Boolean isDelete = false;
+        String currentValue = stringRedisTemplate.opsForValue().get(key);
+        if(StringUtils.isNotBlank(currentValue) && currentValue.equals(value)) {
+            isDelete = stringRedisTemplate.opsForValue().getOperations().delete(key);
+        }
+        return isDelete;
+    }
+}
